9b3afaa035cb7cad94c6bcae2dcfa81441be9fb6
sehraharmeet <150445048+sehraharmeet@users.noreply.github.com>
2024-09-01 10:09:06 +0530

    maven repos

---
+++ src/main/java/com/harmeet/test/TestClass.java
@@ -0,0 +1,8 @@
+package com.harmeet.test;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.print("THis one is from other ones...");
+		//https://github.com/sehraharmeet/mavenrepos.git
+	}
+}
